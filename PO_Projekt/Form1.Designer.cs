da7297eb0622f74acd8e7bf6a63f1cd81409ba76
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-07 20:09:52 +0200

    Dodanie Wyniku i Jedzonka

--- PO_Projekt/Form1.Designer.cs
+++ PO_Projekt/Form1.Designer.cs
@@ -29,13 +29,35 @@ namespace PO_Projekt
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(457, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(98, 21);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Wynik: 0";
+            // 
+            // lblFood
+            // 
+            this.lblFood.BackColor = System.Drawing.Color.Red;
+            this.lblFood.Location = new System.Drawing.Point(287, 139);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(21, 21);
+            this.lblFood.TabIndex = 1;
+            this.lblFood.Text = "\r\n";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.lblFood);
+            this.Controls.Add(this.lblScore);
             this.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
@@ -43,10 +65,14 @@ namespace PO_Projekt
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblFood;
     }
 }
 
